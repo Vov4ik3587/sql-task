de56aec19dcb808308fd85faaeebd2920eca77e6
Vov4ik3587 <galstyan.vova@gmail.com>
2022-07-26 15:13:51 +0700

    fix sql and name of tables, add new goods

--- sql/clothes_shop.sql
+++ sql/clothes_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost
--- Время создания: Июл 26 2022 г., 08:18
+-- Время создания: Июл 26 2022 г., 10:12
 -- Версия сервера: 10.4.24-MariaDB
 -- Версия PHP: 8.1.5
 
@@ -20,47 +20,58 @@ SET time_zone = "+00:00";
 --
 -- База данных: `clothes_shop`
 --
+CREATE DATABASE IF NOT EXISTS `clothes_shop` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `clothes_shop`;
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `catalog`
+-- Структура таблицы `goods`
 --
 
-CREATE TABLE `catalog` (
-  `id_section` int(10) UNSIGNED NOT NULL COMMENT 'идентификатор раздела',
-  `name_section` varchar(25) NOT NULL COMMENT 'название раздела',
-  `description` text NOT NULL COMMENT 'описание раздела',
-  `id_parent_section` int(10) UNSIGNED DEFAULT NULL COMMENT 'идентификатор родительского раздела'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `goods` (
+  `id_good` int(10) UNSIGNED NOT NULL COMMENT 'идентификатор товара',
+  `name_good` varchar(25) NOT NULL COMMENT 'название товара',
+  `id_main_picture` int(10) UNSIGNED NOT NULL COMMENT 'идентификатор главной картинки',
+  `id_main_section` int(10) UNSIGNED NOT NULL COMMENT 'идентификатор основного раздела',
+  `price_discount` int(6) UNSIGNED DEFAULT NULL COMMENT 'цена со скидкой',
+  `price` int(6) UNSIGNED NOT NULL COMMENT 'цена без скидки',
+  `price_promo` int(6) UNSIGNED NOT NULL COMMENT 'цена по промокоду',
+  `description_good` text NOT NULL COMMENT 'описание товара',
+  `availability` tinyint(1) NOT NULL COMMENT 'наличие товара'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='таблица характеристик товара';
 
 --
--- Дамп данных таблицы `catalog`
+-- Дамп данных таблицы `goods`
 --
 
-INSERT INTO `catalog` (`id_section`, `name_section`, `description`, `id_parent_section`) VALUES
-(1, 'Рубашки', 'Рубашки всех брендов, мужские, женские, детские', NULL),
-(2, 'Брюки', 'Брюки, джинсы, мужские, женские', NULL),
-(3, 'Одежда Medicine', 'Одежда бренда Medicine', NULL),
-(4, 'Одежда Tommy', 'Одежда бренда Tommy', NULL);
+INSERT INTO `goods` (`id_good`, `name_good`, `id_main_picture`, `id_main_section`, `price_discount`, `price`, `price_promo`, `description_good`, `availability`) VALUES
+(1, 'Рубашка Medicine One', 1, 3, 2499, 2699, 2227, 'Рубашка Medicine выполнена из вискозной ткани с клетчатым узором.\r\nДетали: прямой крой; отложной воротник; планка и манжеты на пуговицах;\r\nкарман на груди.', 1),
+(2, 'Брюки Tommy One', 4, 6, NULL, 4999, 4499, 'Брюки Tommy из жакарта и ...', 1),
+(3, 'Брюки Medicine One', 4, 5, 3999, 5999, 3499, 'Брюки бренда Medicine', 1),
+(4, 'Рубашка Tommy One', 2, 4, NULL, 6999, 6499, 'Рубашка бренда Tommy', 1),
+(5, 'Рубашка Medicine Two', 2, 3, 2999, 3499, 3199, 'Рубашка Medicine Two, ...', 1),
+(6, 'Рубашка Tommy Two', 2, 4, 2899, 3299, 2999, 'Рубашка Tommy Two, ...', 1),
+(7, 'Брюки Medicine Two', 6, 5, 2999, 3499, 3199, 'Брюки Medicine Two, ...', 1),
+(8, 'Брюки Tommy Two', 4, 6, 2799, 3799, 2699, 'Брюки Tommy Two, ...', 1);
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `goods_picture`
+-- Структура таблицы `good_picture`
 --
 
-CREATE TABLE `goods_picture` (
+CREATE TABLE `good_picture` (
   `ID` int(10) UNSIGNED NOT NULL,
   `id_good` int(10) UNSIGNED NOT NULL COMMENT 'идентификатор товара',
   `id_picture` int(10) UNSIGNED NOT NULL COMMENT 'идентификатор картинки'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Дамп данных таблицы `goods_picture`
+-- Дамп данных таблицы `good_picture`
 --
 
-INSERT INTO `goods_picture` (`ID`, `id_good`, `id_picture`) VALUES
+INSERT INTO `good_picture` (`ID`, `id_good`, `id_picture`) VALUES
 (1, 1, 1),
 (2, 1, 2),
 (3, 1, 3),
@@ -98,30 +109,27 @@ INSERT INTO `picture` (`id_picture`, `path_picture`, `attribute_alt`) VALUES
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `section`
+-- Структура таблицы `sections`
 --
 
-CREATE TABLE `section` (
-  `id_good` int(10) UNSIGNED NOT NULL COMMENT 'идентификатор товара',
-  `name_good` varchar(25) NOT NULL COMMENT 'название товара',
-  `id_main_picture` int(10) UNSIGNED NOT NULL COMMENT 'идентификатор главной картинки',
-  `id_main_section` int(10) UNSIGNED NOT NULL COMMENT 'идентификатор основного раздела',
-  `price_discount` int(6) UNSIGNED DEFAULT NULL COMMENT 'цена со скидкой',
-  `price` int(6) UNSIGNED NOT NULL COMMENT 'цена без скидки',
-  `price_promo` int(6) UNSIGNED NOT NULL COMMENT 'цена по промокоду',
-  `description_good` text NOT NULL COMMENT 'описание товара',
-  `availability` tinyint(1) NOT NULL COMMENT 'наличие товара'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='таблица характеристик товара';
+CREATE TABLE `sections` (
+  `id_section` int(10) UNSIGNED NOT NULL COMMENT 'идентификатор раздела',
+  `name_section` varchar(25) NOT NULL COMMENT 'название раздела',
+  `description` text NOT NULL COMMENT 'описание раздела',
+  `id_parent_section` int(10) UNSIGNED DEFAULT NULL COMMENT 'идентификатор родительского раздела'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Дамп данных таблицы `section`
+-- Дамп данных таблицы `sections`
 --
 
-INSERT INTO `section` (`id_good`, `name_good`, `id_main_picture`, `id_main_section`, `price_discount`, `price`, `price_promo`, `description_good`, `availability`) VALUES
-(1, 'Рубашка Medicine', 1, 1, 2499, 2699, 2227, 'Рубашка Medicine выполнена из вискозной ткани с клетчатым узором.\r\nДетали: прямой крой; отложной воротник; планка и манжеты на пуговицах;\r\nкарман на груди.', 1),
-(2, 'Брюки Tommy', 4, 2, NULL, 4999, 4499, 'Брюки Tommy из жакарта и ...', 1),
-(3, 'Брюки Medicine', 4, 2, 3999, 5999, 3499, 'Брюки бренда Medicine', 1),
-(4, 'Рубашка Tommy', 2, 1, NULL, 6999, 6499, 'Рубашка бренда Tommy', 1);
+INSERT INTO `sections` (`id_section`, `name_section`, `description`, `id_parent_section`) VALUES
+(1, 'Рубашки', 'Рубашки всех брендов, мужские, женские, детские', NULL),
+(2, 'Брюки', 'Брюки, джинсы, мужские, женские', NULL),
+(3, 'Рубашки Medicine', 'Рубашки бренда Medicine', 1),
+(4, 'Рубашки Tommy', 'Рубашки бренда Tommy', 1),
+(5, 'Брюки Medicine', 'Брюки бренда Medicine', 2),
+(6, 'Брюки Tommy', 'Брюки бренда Tommy', 2);
 
 -- --------------------------------------------------------
 
@@ -154,17 +162,17 @@ INSERT INTO `section_good` (`ID`, `id_section`, `id_good`) VALUES
 --
 
 --
--- Индексы таблицы `catalog`
+-- Индексы таблицы `goods`
 --
-ALTER TABLE `catalog`
-  ADD PRIMARY KEY (`id_section`),
-  ADD UNIQUE KEY `id_section_3` (`id_section`),
-  ADD KEY `id_section` (`id_section`);
+ALTER TABLE `goods`
+  ADD PRIMARY KEY (`id_good`),
+  ADD KEY `id_main_picture` (`id_main_picture`),
+  ADD KEY `id_main_section` (`id_main_section`);
 
 --
--- Индексы таблицы `goods_picture`
+-- Индексы таблицы `good_picture`
 --
-ALTER TABLE `goods_picture`
+ALTER TABLE `good_picture`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `id_good` (`id_good`),
   ADD KEY `id_picture` (`id_picture`);
@@ -176,12 +184,12 @@ ALTER TABLE `picture`
   ADD PRIMARY KEY (`id_picture`);
 
 --
--- Индексы таблицы `section`
+-- Индексы таблицы `sections`
 --
-ALTER TABLE `section`
-  ADD PRIMARY KEY (`id_good`),
-  ADD KEY `id_main_picture` (`id_main_picture`),
-  ADD KEY `id_main_section` (`id_main_section`);
+ALTER TABLE `sections`
+  ADD PRIMARY KEY (`id_section`),
+  ADD UNIQUE KEY `id_section_3` (`id_section`),
+  ADD KEY `id_section` (`id_section`);
 
 --
 -- Индексы таблицы `section_good`
@@ -196,15 +204,15 @@ ALTER TABLE `section_good`
 --
 
 --
--- AUTO_INCREMENT для таблицы `catalog`
+-- AUTO_INCREMENT для таблицы `goods`
 --
-ALTER TABLE `catalog`
-  MODIFY `id_section` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'идентификатор раздела', AUTO_INCREMENT=5;
+ALTER TABLE `goods`
+  MODIFY `id_good` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'идентификатор товара', AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT для таблицы `goods_picture`
+-- AUTO_INCREMENT для таблицы `good_picture`
 --
-ALTER TABLE `goods_picture`
+ALTER TABLE `good_picture`
   MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
@@ -214,10 +222,10 @@ ALTER TABLE `picture`
   MODIFY `id_picture` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'идентификатор картинки', AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT для таблицы `section`
+-- AUTO_INCREMENT для таблицы `sections`
 --
-ALTER TABLE `section`
-  MODIFY `id_good` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'идентификатор товара', AUTO_INCREMENT=5;
+ALTER TABLE `sections`
+  MODIFY `id_section` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'идентификатор раздела', AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT для таблицы `section_good`
@@ -230,25 +238,25 @@ ALTER TABLE `section_good`
 --
 
 --
--- Ограничения внешнего ключа таблицы `goods_picture`
+-- Ограничения внешнего ключа таблицы `goods`
 --
-ALTER TABLE `goods_picture`
-  ADD CONSTRAINT `goods_picture_ibfk_1` FOREIGN KEY (`id_good`) REFERENCES `section` (`id_good`),
-  ADD CONSTRAINT `goods_picture_ibfk_2` FOREIGN KEY (`id_picture`) REFERENCES `picture` (`id_picture`);
+ALTER TABLE `goods`
+  ADD CONSTRAINT `goods_ibfk_1` FOREIGN KEY (`id_main_picture`) REFERENCES `picture` (`id_picture`),
+  ADD CONSTRAINT `goods_ibfk_2` FOREIGN KEY (`id_main_section`) REFERENCES `sections` (`id_section`);
 
 --
--- Ограничения внешнего ключа таблицы `section`
+-- Ограничения внешнего ключа таблицы `good_picture`
 --
-ALTER TABLE `section`
-  ADD CONSTRAINT `section_ibfk_1` FOREIGN KEY (`id_main_picture`) REFERENCES `picture` (`id_picture`),
-  ADD CONSTRAINT `section_ibfk_2` FOREIGN KEY (`id_main_section`) REFERENCES `catalog` (`id_section`);
+ALTER TABLE `good_picture`
+  ADD CONSTRAINT `good_picture_ibfk_1` FOREIGN KEY (`id_good`) REFERENCES `goods` (`id_good`),
+  ADD CONSTRAINT `good_picture_ibfk_2` FOREIGN KEY (`id_picture`) REFERENCES `picture` (`id_picture`);
 
 --
 -- Ограничения внешнего ключа таблицы `section_good`
 --
 ALTER TABLE `section_good`
-  ADD CONSTRAINT `section_good_ibfk_1` FOREIGN KEY (`id_good`) REFERENCES `section` (`id_good`),
-  ADD CONSTRAINT `section_good_ibfk_2` FOREIGN KEY (`id_section`) REFERENCES `catalog` (`id_section`);
+  ADD CONSTRAINT `section_good_ibfk_1` FOREIGN KEY (`id_good`) REFERENCES `goods` (`id_good`),
+  ADD CONSTRAINT `section_good_ibfk_2` FOREIGN KEY (`id_section`) REFERENCES `sections` (`id_section`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
